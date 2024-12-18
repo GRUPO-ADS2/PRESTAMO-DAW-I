d7dc7a449faf664ceff1aba3c804a249ed942ba3
jhonatan <jhon2226g@gmail.com>
2024-12-18 12:26:30 -0500

    ian god

---
+++ ApiRestPrestamoEdu/target/classes/init.sql
@@ -0,0 +1,20 @@
+USE BD_APELLIDO;
+insert into tb_tipo_servicio (des_tiposerv) values
+	(atención preventiva),
+	(tratamientos correctivos),
+	(procedimientos estéticos),
+	(cuidados de urgencia),
+	(limpiezas dentales),
+	(extracciones),
+	(tratamientos de caries),
+	(ortodoncia),
+	(blanqueamiento dental),
+	(implantes),
+	(cuidados periodontales),
+	(prótesis),
+	(coronas),
+	(endodoncia),
+	(evaluación de desórdenes de la ATM),
+	(consultas regulares),
+	(planificación de cuidados a largo plazo),
+	(rehabilitación oral);
